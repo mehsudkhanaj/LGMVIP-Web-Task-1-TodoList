fb64948ab0aff3644af68f41be4e245f7d11215d
Jamshed Mehsud <91126856+mehsudkhanaj@users.noreply.github.com>
2023-06-05 17:22:36 +0500

    Add files via upload
    todolist final testing

---
+++ build/precache-manifest.6792eac9aa2c89b41833bf6611d055ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea8a47068a1c868d55e1",
+    "url": "/static/css/main.7d110a0c.chunk.css"
+  },
+  {
+    "revision": "ea8a47068a1c868d55e1",
+    "url": "/static/js/main.ea8a4706.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "728a4c2fb8bc601804e2",
+    "url": "/static/js/2.728a4c2f.chunk.js"
+  },
+  {
+    "revision": "7b03630978f7619e1dae",
+    "url": "/static/js/runtime~main.7b036309.js"
+  },
+  {
+    "revision": "264b7b4efe798c28234aeb8053daa372",
+    "url": "/index.html"
+  }
+];
